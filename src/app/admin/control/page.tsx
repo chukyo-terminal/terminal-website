4cf7472afdacb64cf64e4eac91978e1e585420f2
KATO Hayate <dev@hayatek.jp>
2025-08-18 20:44:44 +0900

    feat: rootユーザーへの権限昇格機能を実装

---
+++ src/app/admin/control/page.tsx
@@ -0,0 +1,15 @@
+import type { JSX } from 'react';
+
+
+/**
+ * サイト管理ページを生成する。
+ *
+ * @returns 生成されたサイト管理ページの要素
+ */
+export default function ControlPage(): JSX.Element {
+  return (
+    <div className="mx-auto px-4 py-8">
+      <h1 className="md:mt-8 text-2xl font-bold mb-4">サイト管理</h1>
+    </div>
+  );
+}
